b6ec94c9d0c087134d9579dcad3b465e87daa258
student <sveta.vorobueva@gmail.com>
2023-02-09 18:17:46 +0300

    dthdtdtgdfj

--- Воробьева Света/main.py
+++ Воробьева Света/main.py
@@ -3,7 +3,7 @@ Exercises
 1. How do you make the snake faster or slower?
 2. How can you make the snake go around the edges?
 3. How would you move the food?
-4. Change the snake to responro mouse clicks.
+4. Change the snake to retyonro mouse clicks.
 """
 '''fgfhfh'''
 from random import randrange
